afc5c7cb76e3a71b8d9fdff7b6f23f9c70500a21
simonepri <simonepri@outlook.com>
2020-09-25 21:48:24 +0200

    Fix lint errors

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,12 +1,12 @@
 const olinfo = require('../libs/olinfo');
 
-module.exports = async (req, res) => {
-  const taskname = req.query.taskname;
+module.exports = async (request, response) => {
+  const taskname = request.query.taskname;
   let data;
   try {
     data = await olinfo.task(taskname);
   } catch (error) {
-    return res.status(error.status).json({error: error.message});
+    return response.status(error.status).json({error: error.message});
   }
-  return res.status(200).json(data);
-}
+  return response.status(200).json(data);
+};
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,30 +1,30 @@
 const got = require('got');
 const olinfo = require('../libs/olinfo');
 
-function getAPIHostAndPort(req) {
-  let proto = req.connection.encrypted ? 'https' : 'http';
-  proto = req.headers['x-forwarded-proto'] || proto;
+function getAPIHostAndPort(request) {
+  let proto = request.connection.encrypted ? 'https' : 'http';
+  proto = request.headers['x-forwarded-proto'] || proto;
   proto = proto.split(/\s*,\s*/)[0];
-  return `${proto}://${req.headers.host}`;
+  return `${proto}://${request.headers.host}`;
 }
 
-module.exports = async (req, res) => {
-  const username = req.query.username;
+module.exports = async (request, response) => {
+  const username = request.query.username;
   let data;
   try {
     data = await olinfo.user(username);
   } catch (error) {
-    return res.status(error.status).json({error: error.message});
+    return response.status(error.status).json({error: error.message});
   }
 
   const promises = [];
   data.scores.forEach(problem => {
     problem.multiplier = 0;
     const promise = got.get(`task/${problem.name}`, {
-      baseUrl: getAPIHostAndPort(req),
+      baseUrl: getAPIHostAndPort(request),
       json: true
-    }).then(response => {
-      problem.multiplier = response.body.score_multiplier;
+    }).then(result => {
+      problem.multiplier = result.body.score_multiplier;
     }).catch(error => {
       console.error(error);
     });
@@ -32,11 +32,11 @@ module.exports = async (req, res) => {
   });
   await Promise.all(promises);
 
-  res.status(200).json({
+  response.status(200).json({
     name: `${data.first_name} ${data.last_name}`,
     username: data.username,
     mailhash: data.mail_hash,
     score: data.score,
     problems: data.scores
   });
-}
+};
